2ba51854432e4fb9df5bb2ee54c81ed54a294d4b
heyipomoea <heyipomoea@gmail.com>
2023-11-22 16:14:41 +0800

    Update 打包完成

---
+++ Assets/Script/HitSystem.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+
+
+namespace Heyipomoea
+{
+    /// <summary>
+    /// 打擊系統
+    /// </summary>
+    public class HitSystem : MonoBehaviour
+    {
+        [SerializeField, Header("上方區域位移")]
+        private Vector3 offsetUp;
+        [SerializeField, Header("上方區域尺寸")]
+        private Vector3 sizeUp = Vector3.one;
+        [SerializeField, Header("下方區域位移")]
+        private Vector3 offsetDown;
+        [SerializeField, Header("下方區域尺寸")]
+        private Vector3 sizeDown = Vector3.one;
+        [SerializeField, Header("可以打擊圖層")]
+        private LayerMask layerHit;
+
+        private Vector3 pointUp => transform.position + transform.TransformDirection(offsetUp);
+        private Vector3 pointDown => transform.position + transform.TransformDirection(offsetDown);
+
+        private bool isHitUp;
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.matrix = Matrix4x4.TRS(
+                pointUp, 
+                transform.rotation, 
+                transform.localScale);
+
+            Gizmos.color = new Color(0.5f, 1, 0.5f, 0.7f);
+            Gizmos.DrawCube(Vector3.zero, sizeUp);
+
+            Gizmos.matrix = Matrix4x4.TRS(
+                pointDown,
+                transform.rotation,
+                transform.localScale);
+
+            Gizmos.color = new Color(0.8f, 0.8f, 0.5f, 0.7f);
+            Gizmos.DrawCube(Vector3.zero, sizeDown);
+        }
+
+        private void Update()
+        {
+            CheckHitFlow();
+        }
+
+        private void CheckHitFlow()
+        {
+            if (!isHitUp) isHitUp = CheckHit(pointUp, sizeUp);
+            else if (CheckHit(pointDown, sizeDown))
+            {
+                ScoreManager.instance.AddScoreAndPlaySound(1.5f, 2.5f);
+                Destroy(gameObject);
+            }
+        }
+
+        /// <summary>
+        /// 打擊判定
+        /// </summary>
+        /// <param name="center">中心點</param>
+        /// <param name="size">尺寸</param>
+        /// <returns>是否被打到</returns>
+        private bool CheckHit(Vector3 center, Vector3 size)
+        {
+            Collider[] hits = Physics.OverlapBox(center, size / 2, transform.rotation, layerHit);
+            return hits.Length > 0;
+        }
+    }
+}
+
---
+++ Assets/Script/ScoreManager.cs
@@ -0,0 +1,43 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Heyipomoea
+{
+    /// <summary>
+    /// 分數管理器:打擊到方塊後播放音效及累加分數
+    /// </summary>
+    [RequireComponent(typeof(AudioSource))]
+    public class ScoreManager : MonoBehaviour
+    {
+        public static ScoreManager instance;
+
+        [SerializeField, Header("音效")]
+        private AudioClip soundHit;
+        [SerializeField, Header("分數文字")]
+        private TextMeshProUGUI textScore;
+
+        private AudioSource aud;
+        private int score = 0;
+        private int socrePerHit = 10;
+
+        private void Awake()
+        {
+            instance = this;
+            aud = GetComponent<AudioSource>();
+        }
+
+        /// <summary>
+        /// 添加分數並播放音效
+        /// </summary>
+        /// <param name="min">最小音量</param>
+        /// <param name="max">最大音量</param>
+        public void AddScoreAndPlaySound(float min, float max)
+        {
+            float randomVolume = Random.Range(min, max);
+            aud.PlayOneShot(soundHit, randomVolume);
+            score += socrePerHit;
+            textScore.text = score.ToString();
+        }
+    }
+}
+
